d5daf7725996488305cef21e70968045569663de
JobHaver <cajhbru@gmail.com>
2023-03-02 18:36:42 -0800

    Update find-the-index-of-the-first-occurrence-in-a-string.cpp
    reading problem description it turns out needle.size() cannot be less than 1 so check for 0 is unneeded

--- find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         int i, j;
-        if(needle.size() == 0)
-            return -1;
 
         for(i = 0; i + needle.size() <= haystack.size(); i++){
             if(haystack[i] == needle[0] && haystack[i+needle.size()-1] == needle[needle.size()-1]){
