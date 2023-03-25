c6c51b3a7da059043b50e7377acac9b94b5636b9
JobHaver <cajhbru@gmail.com>
2023-03-24 19:56:43 -0700

    Update count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
    just streamlining some stuff, only real change is moving the push_back({}) to (1) in initialization.

--- count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     long long countPairs(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> groups;
+        vector<vector<int>> groups(1);
         vector<int> nodes(n);
         int group = 1;
         long long ans = 0;
-
-        groups.push_back({}); //done so we can use group 1 desination
 
         for(const vector<int> &v : edges){
             int &n0 = nodes[v[0]];
@@ -15,33 +13,22 @@ public:
             if(n0 && n1){
                 if(n0 != n1){
                     int temp = n1;
-                    for(int &n : groups[temp]){ // overwrite group n1
-                        nodes[n]= n0;
-                        groups[n0].push_back(n);
-                    }
+                    for(int &n : groups[temp]) // overwrite group n1
+                        nodes[n]= n0, groups[n0].push_back(n);
 
                     groups[temp].clear();
                 }
             }
-            else if(n0){
-                n1 = n0;
-                groups[n1].push_back(v[1]);
-            }
-            else if(n1){
-                n0 = n1;
-                groups[n1].push_back(v[0]);
-            }
-            else{
-                n0 = group++;
-                n1 = n0;
-                groups.push_back({v[1], v[0]});
-            }
+            else if(n0)
+                n1 = n0, groups[n1].push_back(v[1]);
+            else if(n1)
+                n0 = n1, groups[n1].push_back(v[0]);
+            else
+                n0 = group++, n1 = n0, groups.push_back({v[1], v[0]});
         }
 
-        for(const vector<int> &v : groups){
-            n -= v.size();
-            ans += v.size() * n;
-        }
+        for(const vector<int> &v : groups)
+            n -= v.size(), ans += v.size() * n;
 
         while(n-- > 1)
             ans += n;
