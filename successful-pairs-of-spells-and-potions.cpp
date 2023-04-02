a344f131c74739f27918a25f1ceec8884f2e2a22
JobHaver <cajhbru@gmail.com>
2023-04-01 21:36:12 -0700

    Create successful-pairs-of-spells-and-potions.cpp

---
+++ successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    int search(vector<int>& potions, long long success, long long spell) {
+        int left = 0, right = potions.size(), middle;
+
+        while(left < right){
+            middle = (left + right) / 2;
+
+            if(potions[middle] * spell < success)
+                left = middle + 1;
+            else
+                right = middle;
+        }
+
+        return left;
+    }
+
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+
+        for(int &spell : spells)
+            spell = potions.size() - search(potions, success, spell);
+
+        return spells;
+    }
+};
