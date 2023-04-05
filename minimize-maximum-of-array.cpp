77d7afcd95b2c47b1b4dcf46cc8909f5fad1c5df
JobHaver <cajhbru@gmail.com>
2023-04-05 15:02:58 -0700

    Update minimize-maximum-of-array.cpp
    simplified avg calc, was dividing twice, was unnecessary

--- minimize-maximum-of-array.cpp
+++ minimize-maximum-of-array.cpp
@@ -4,7 +4,7 @@ public:
         int max = 0;
         double avg = 0;
         for(int i = 0; i < nums.size(); i++){
-            avg = ((avg * i) / (i+1.0)) + (nums[i] / (i+1.0));
+            avg = ((avg * i) + nums[i]) / (i+1.0);
             if(avg > max)
                 max = ceil(avg);
         }
