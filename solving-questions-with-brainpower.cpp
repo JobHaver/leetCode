7b43c6e795fb31f8e8eb2235fe0580c1747411b2
JobHaver <cajhbru@gmail.com>
2023-05-11 18:04:28 -0700

    Update solving-questions-with-brainpower.cpp
    less readable, more better

--- solving-questions-with-brainpower.cpp
+++ solving-questions-with-brainpower.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     long long mostPoints(vector<vector<int>>& questions) {
-        vector<long long> ans(questions.size()+1);
-        long long size = questions.size(), high = 0;
+        int size = questions.size();
+        vector<long long> ans(size+1);
+        long long high = 0;
 
         for(int i = 0; i < questions.size(); i++){
-            long long score = questions[i][0], cost = questions[i][1], &question = ans[min(i+cost+1, size)];
+            long long &question = ans[min(i+questions[i][1]+1, size)];
             high = max(ans[i], high);
-            question = max(question, high + score);
+            question = max(question, high + questions[i][0]);
         }
 
-        return max(ans[questions.size()], ans[questions.size()-1]);
+        return ans[size];
     }
 };
