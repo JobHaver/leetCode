4a67461b562e4b73439169afcbe660235049fb75
JobHaver <cajhbru@gmail.com>
2023-06-06 17:53:09 -0700

    Update minimum-flips-to-make-a-or-b-equal-to-c.cpp

--- minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -4,16 +4,14 @@ public:
         int flips = 0;
 
         for(; a || b || c; a>>=1, b>>=1, c>>=1){
-            bool aB = a&1, bB = b&1, cB = c&1;
-
-            if(cB){
-                if(!(aB || bB))
+            if(c&1){
+                if(!(a&1 || b&1))
                     flips++;
             }
             else{
-                if(aB)
+                if(a&1)
                     flips++;
-                if(bB)
+                if(b&1)
                     flips++;
             }
         }
