e0a319068060605d75fdd1fbd96978b2a27ff82f
JobHaver <cajhbru@gmail.com>
2023-06-08 18:42:51 -0700

    Update find-smallest-letter-greater-than-target.cpp

--- find-smallest-letter-greater-than-target.cpp
+++ find-smallest-letter-greater-than-target.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        int i = upper_bound(letters.begin(), letters.end(), target) - letters.begin();
-        return i < letters.size() ? letters[i] : letters[0];
+        vector<char>::iterator i = upper_bound(letters.begin(), letters.end(), target);
+        return i == letters.end() ? letters[0] : *i;
     }
 };
