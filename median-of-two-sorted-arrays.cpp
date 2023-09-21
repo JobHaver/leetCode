1ec6ce8f2e8846ef54730db1d63ca8e799db1936
JobHaver <cajhbru@gmail.com>
2023-09-20 17:58:50 -0700

    Update median-of-two-sorted-arrays.cpp
    clean up from last time I completed this problem

--- median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> combined;
-        int v1i = 0, v2i = 0;
         int size1 = nums1.size(), size2 = nums2.size(), mid = (size1+size2)/2;
-
-        combined.reserve(mid+1);
+        int combined[mid + 1];
+        int v1i = 0, v2i = 0;
         
-        while(mid >= combined.size()){
+        for(int i = 0; mid >= i; i++){
             if(v1i == size1)
-                combined.push_back(nums2[v2i++]);
+                combined[i] = nums2[v2i++];
             else if(v2i == size2)
-                combined.push_back(nums1[v1i++]);
-            else{
-                if(nums1[v1i] < nums2[v2i])
-                    combined.push_back(nums1[v1i++]);
-                else
-                    combined.push_back(nums2[v2i++]);
-            }
+                combined[i] = nums1[v1i++];
+            else if(nums1[v1i] < nums2[v2i])
+                combined[i] = nums1[v1i++];
+            else
+                combined[i] = nums2[v2i++];
         }
 
-        if ((size1+size2) % 2 == 0)
-            return double ((combined[mid] + combined[mid - 1])/2.0);
-        else
-            return double (combined[mid]);
+        return (size1+size2) % 2 == 0 ? double ((combined[mid] + combined[mid - 1])/2.0) : double (combined[mid]);
     }
 };
