10ca829ff0a4af16b4dc7802a2948c4625fec40a
JobHaver <cajhbru@gmail.com>
2023-11-10 21:21:48 -0800

    Create design-graph-with-shortest-path-calculator.cpp

---
+++ design-graph-with-shortest-path-calculator.cpp
@@ -0,0 +1,50 @@
+class Graph {
+private:
+    vector<vector<pair<int, int>>> edges;
+
+public:
+    Graph(int n, vector<vector<int>>& e) {
+        edges.resize(n);
+        for(vector<int> &v : e)
+            edges[v[0]].push_back({v[1], v[2]});
+    }
+    
+    void addEdge(vector<int> v) {
+        edges[v[0]].push_back({v[1], v[2]});
+    }
+    
+    int shortestPath(int node1, int node2) {
+        int ans = INT_MAX;
+        vector<int> costs(edges.size(), -1);
+        queue<int> next;
+
+        next.push(node1);
+        costs[node1] = 0;
+        while(!next.empty()){
+            int curr = next.front();
+            next.pop();
+            
+            if(costs[curr] >= ans)
+                continue;
+                
+            for(pair<int, int> &p : edges[curr]){
+                if(costs[p.first] == -1 || costs[p.first] > costs[curr] + p.second){
+                    costs[p.first] = costs[curr] + p.second;
+                    if(p.first == node2)
+                        ans = costs[p.first];
+                    else
+                        next.push(p.first);
+                }
+            }
+        }
+
+        return costs[node2];
+    }
+};
+
+/**
+ * Your Graph object will be instantiated and called as such:
+ * Graph* obj = new Graph(n, edges);
+ * obj->addEdge(edge);
+ * int param_2 = obj->shortestPath(node1,node2);
+ */
