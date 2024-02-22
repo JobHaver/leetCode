0600cc5427ffd9df5e70350e5aff2bc69486d914
JobHaver <cajhbru@gmail.com>
2024-02-21 16:23:50 -0800

    Update missing-number.cpp
    removes need for sort function

--- missing-number.cpp
+++ missing-number.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size(); i++)
-            if(nums[i] != i)
-                return i;
-        
-        return nums.size();
+        int actual = 0, expected = nums.size();
+        for(int i = 0; i < nums.size(); expected += i++)
+            actual += nums[i];
+
+        return expected - actual;
     }
 };
