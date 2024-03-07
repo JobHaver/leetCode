8cb5f762d04900cb63b89f0a57e3003224ce8a73
JobHaver <cajhbru@gmail.com>
2024-03-06 16:13:18 -0800

    Create middle-of-the-linked-list.cs

---
+++ middle-of-the-linked-list.cs
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        ListNode fast, slow;
+        for(fast = slow = head; fast != null && fast.next != null; slow = slow.next, fast = fast.next.next);
+        
+        return slow;
+    }
+}
