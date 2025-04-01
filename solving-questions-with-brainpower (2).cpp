1296482250c41f6a66f1583489c6a68e64e1ebad
JobHaver <cajhbru@gmail.com>
2025-04-01 10:38:46 -0700

    Update solving-questions-with-brainpower (2).cpp
    changed for naming consistency

--- solving-questions-with-brainpower (2).cpp
+++ solving-questions-with-brainpower (2).cpp
@@ -6,10 +6,10 @@ public:
         vector<long long> previousPoints(questions.size());
         for(int i = questions.size()-1; i >= 0; i--){
             int nextQuestion = questions[i][1] + i + 1;
-            long long currScore = questions[i][0] + (nextQuestion < questions.size() ? previousPoints[nextQuestion] : 0);
+            long long currpoints = questions[i][0] + (nextQuestion < questions.size() ? previousPoints[nextQuestion] : 0);
 
-            if(currScore > ans)
-                ans = currScore;
+            if(currpoints > ans)
+                ans = currpoints;
 
             previousPoints[i] = ans;
         }
