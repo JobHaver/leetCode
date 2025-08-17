0b891a879aec68357bdaf7da92c1d4571913e9bc
JobHaver <cajhbru@gmail.com>
2025-08-17 12:24:33 -0700

    After looking at previous solution, realized the 0 check was unneeded as curr <= n takes care of that

--- power-of-four (2).cpp
+++ power-of-four (2).cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n <= 0)
-            return false;
-
         for(long long curr = 1; curr <= n; curr *= 4)
             if(curr == n)
                 return true;
--- power-of-three.cpp
+++ power-of-three.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n <= 0)
-            return false;
-
         for(long long curr = 1; curr <= n; curr *= 3)
             if(curr == n)
                 return true;
