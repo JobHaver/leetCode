32e9ab9d55833af23a5932a6041d80e065e90691
JobHaver <cajhbru@gmail.com>
2026-02-24 17:14:43 -0800

    Create sort-integers-by-the-number-of-1-bits (2).cpp

---
+++ sort-integers-by-the-number-of-1-bits (2).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](const int& a, const int& b) {
+            int bitcountA = __builtin_popcount(a);
+            int bitcountB = __builtin_popcount(b);
+
+            if(bitcountA == bitcountB)
+                return a < b;
+
+            return bitcountA < bitcountB;
+        });
+
+        return arr;
+    }
+};
