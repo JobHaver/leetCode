ecccfd160e0417d43d80c349586b19159fe0dd76
JobHaver <cajhbru@gmail.com>
2026-04-21 18:45:37 -0700

    Update two-furthest-houses-with-different-colors.cpp
    2n vs n^2

--- two-furthest-houses-with-different-colors.cpp
+++ two-furthest-houses-with-different-colors.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     int maxDistance(vector<int>& colors) {
         int ans = 0;
-        for(int l = 0; l < colors.size() && colors.size() - l > ans; l++){
-            for(int r = colors.size()-1; r > l && r - l > ans; r--){
-                if(colors[l] != colors[r]){
-                    ans = max(ans, r-l);
+        for(int l = 0, r = colors.size()-1; l < r; r--){
+            if(colors[l] != colors[r]){
+                ans = max(ans, r-l);
                     break;
-                }
+            }
+        }
+
+        for(int l = 0, r = colors.size()-1; l < r; l++){
+            if(colors[l] != colors[r]){
+                ans = max(ans, r-l);
+                    break;
             }
         }
 
