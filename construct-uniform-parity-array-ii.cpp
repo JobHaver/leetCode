67e70d2badd139d590487496ae9df8570ed2e5b7
JobHaver <cajhbru@gmail.com>
2026-09-02 22:38:31 -0700

    Create construct-uniform-parity-array-ii.cpp

---
+++ construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int minOdd = INT_MAX;
+        for(int i = 0; i < nums1.size(); i++)
+            if(nums1[i] & 1)
+                minOdd = min(minOdd, nums1[i]);
+
+        if(minOdd == INT_MAX)
+            return true;
+
+        for(int i = 0; i < nums1.size(); i++)
+            if(!(nums1[i] & 1) && nums1[i] - minOdd <= 0)
+                return false;
+
+        return true;
+    }
+};
